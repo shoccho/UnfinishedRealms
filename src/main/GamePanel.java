09224612dc5dd601b8ed2c29f75175d6d3d409f9
shoccho <sfb.shoccho@gmail.com>
2024-08-10 12:25:20 +0600

    Created game loop and keyhandler

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,7 +1,6 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 
 import javax.swing.JPanel;
 
@@ -18,13 +17,23 @@ public class GamePanel extends JPanel implements Runnable{
 
     int screenWidth = tileSize * maxScreenCol;
     int screenHeight = tileSize * maxScreenRow;
+    int FPS = 60;
 
     Thread gameThread;
+    KeyHandler keyHandler;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 5;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.DARK_GRAY);
         this.setDoubleBuffered(true);
+        this.keyHandler = new KeyHandler();
+        this.addKeyListener(this.keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -34,6 +43,43 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Having PHUN XD");
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+            if(delta >=1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update(){
+        if(keyHandler.up){
+            playerY -= playerSpeed;
+        }
+        if(keyHandler.down){
+            playerY += playerSpeed;
+        }
+        if(keyHandler.left){
+            playerX -= playerSpeed;
+        }
+        if(keyHandler.right){
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
     }
 }
---
+++ src/main/KeyHandler.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public boolean up, down, left, right;
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        int code = keyEvent.getKeyCode();
+        switch (code){
+            case KeyEvent.VK_W -> {
+                up = true;
+            }
+            case KeyEvent.VK_S -> {
+                down = true;
+            }
+            case KeyEvent.VK_A -> {
+                left = true;
+            }
+            case KeyEvent.VK_D -> {
+                right = true;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        int code = keyEvent.getKeyCode();
+        switch (code) {
+            case KeyEvent.VK_W -> {
+                up = false;
+            }
+            case KeyEvent.VK_S -> {
+                down = false;
+            }
+            case KeyEvent.VK_A -> {
+                left = false;
+            }
+            case KeyEvent.VK_D -> {
+                right = false;
+            }
+        }
+    }
+}
